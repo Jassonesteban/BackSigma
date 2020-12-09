699893fff104b35fe9421d7ea62c0e1562dc0f6d
Jasson Esteban <jgualguanguzman@gmail.com>
2020-12-08 21:25:42 -0500

    Segundos cambios en el test

--- src/test/java/com/mobile/appuebliar/AppuebliarApplicationTests.java
+++ src/test/java/com/mobile/appuebliar/AppuebliarApplicationTests.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.mobile.sigma.Sigma;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+@SpringBootTest(classes = Sigma.class)
 class AppuebliarApplicationTests {
 
 	@Test
